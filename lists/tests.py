412c2e244f11cd411b86ef06cdc6539d2b4915e5
Tian Gao <tiangao.apps@gmail.com>
2014-06-18 02:14:22 -0400

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 
 from .views import home_page
+from .models import Item
 
 
 class HomePageTest(TestCase):
@@ -27,8 +28,51 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
 
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': 'A new list item'})
-        self.assertEquals(response.content.decode(), expected_html)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_home_page_redirects_after_post(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemery 1')
+        Item.objects.create(text='itemery 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemery 1', response.content.decode())
+        self.assertIn('itemery 2', response.content.decode())
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')

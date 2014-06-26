317ba9c63cd81400d077c91c3c891cef051ba094
Tian Gao <tiangao.apps@gmail.com>
2014-06-25 23:58:05 -0400

    Moved FTs into their own individual files

---
+++ functional_tests/base.py
@@ -0,0 +1,35 @@
+import sys
+from django.contrib.staticfiles.testing import StaticLiveServerCase
+from selenium import webdriver
+
+
+class FunctionalTest(StaticLiveServerCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
+    def setUp(self):
+        self.browser = self.start_chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def start_chrome(self):
+        return webdriver.Chrome('/Users/bowenfeng/dev/chromedriver')
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])

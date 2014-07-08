2828334c6006dca710ec10d9cc4e06d12316e638
Tian Gao <tiangao.apps@gmail.com>
2014-07-08 00:26:11 -0400

    Test important persona fail response is logged

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -3,6 +3,8 @@ from django.contrib.auth import get_user_model
 import requests
 import logging
 User = get_user_model()
+logger = logging.getLogger(__name__)
+
 
 PERSONA_VERIFY_URL = 'https://verifier.login.persona.org/verify'
 
@@ -10,19 +12,18 @@ PERSONA_VERIFY_URL = 'https://verifier.login.persona.org/verify'
 class PersonaAuthenticationBackend(object):
 
     def authenticate(self, assertion):
-        logging.warning('entering authenticate function')
         response = requests.post(
             PERSONA_VERIFY_URL,
             data={'assertion': assertion, 'audience': settings.DOMAIN}
         )
-        logging.warning('got response from persona')
-        logging.warning(response.content.decode())
         if response.ok and response.json()['status'] == 'okay':
             email = response.json()['email']
             try:
                 return User.objects.get(email=email)
             except User.DoesNotExist:
                 return User.objects.create(email=email)
+        else:
+            logger.warning('Persona says no. Json was: {}'.format(response.json()))
 
     def get_user(self, email):
         try:
